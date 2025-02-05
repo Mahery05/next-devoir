f57db4585f1d1a75c3eed33ee525089ab71ab494
Cedric <cedric.jamme@edu.devinci.fr>
2025-02-05 15:26:16 +0100

    add filter

--- app/activites/page.tsx
+++ app/activites/page.tsx
@@ -1,11 +1,13 @@
 import Activites from "@/components/Activites";
+import { Suspense } from "react";
 
 export default function ActivitesPage() {
- return (
-     <>
-       <h2>Activitées</h2>
-       <Activites/>
-     </>
-   );
-  
+  return (
+    <>
+      <h2>Activités</h2>
+      <Suspense fallback={<p>Chargement...</p>}>
+        <Activites />
+      </Suspense>
+    </>
+  );
 }
